816816bb5b5bdca44310a100fdb62031b088464e
Booooooomy <globalman96@naver.com>
2017-10-28 11:11:40 -0700

    Update Courseschedule.cpp

--- Courseschedule.cpp
+++ Courseschedule.cpp
@@ -1,9 +1,9 @@
 #include "CourseSchedule.h"
 
-CourseSchedule::CourseSchedule(string studentName, Semester semest, int num): sname(studentName), smester(semest), maxSize(num)
+CourseSchedule::CourseSchedule(string studentName, Semester semest, int num) : sname(studentName), smester(semest), maxSize(num)
 {
-	numCourses = 0;
-	Courses = new Course[maxSize];
+	CourseSchedule::numCourses = 0;
+	Courses = new Course[maxSize];					// compile error!
 }
 
 CourseSchedule::~CourseSchedule()
@@ -23,7 +23,7 @@ Semester CourseSchedule::getSemester() const
 
 int CourseSchedule::getnumCourse() const
 {
-	return numCourses;
+	return CourseSchedule::numCourses;
 }
 
 void CourseSchedule::setStudentName(string sn)
@@ -31,7 +31,72 @@ void CourseSchedule::setStudentName(string sn)
 	sname = sn;
 }
 
-void CourseSchedule::checkDates(Semester, Date, Date)
+
+// utility function 
+int CourseSchedule::checkDates(Semester sem, Date sDate, Date eDate)
 {
-	if ()
+	// count if start or end date satisfies the semester duration 
+	int sCnt = 0;
+	int eCnt = 0;
+
+	// start  end       o:within 
+	//  o      o  --5 -->clear
+	//  x      o  --3 -->startD
+	//  o      x  --4 -->endD
+	//  x      x  --2 -->both
+	sCnt = ((sem.getSemStartDate < sDate) ? 4 : 2);
+	eCnt = ((sem.getSemEndDate > eDate) ? 1 : 0);
+
+	return sCnt + eCnt;
+}
+
+void CourseSchedule::addCourse(Course& cs, Semester sem, Date sDate, Date eDate)
+{
+	//allow adding a course only when the dates are within the semester duration
+	if (checkDates(sem, sDate, eDate) == 5)
+	{
+		Courses[numCourses] = cs;			// compile error!
+		numCourses++;
+	}
+}
+
+void CourseSchedule::removeCourse()
+{
+	char sel;
+	cout << "Do you want to remove any course on the schedule?" << endl;
+	cout << "if yes, press 'y'. if not, press 'n'" << endl;
+	cin >> sel;
+
+	do
+	{
+		if (sel == 'y')
+		{
+			// remove!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		}
+		else if (sel == 'n')
+		{
+		}
+		else
+		{
+			cout << "Invalid selection" << endl;
+			cout << "Do you want to remove any course on the schedule?" << endl;
+			cout << "if yes, press 'y'. if not, press 'n'" << endl;
+			cin >> sel;
+		}
+	} while (sel != 'n');
+}
+
+ostream &operator<<(ostream & output, const CourseSchedule & sched)
+{
+	output << "CLASS SCHEDULE\n" << "-----------------------\n" << "Name: " << sched.sname
+		<< endl << "Semester: " << sched.smester
+		<< endl << "Number of Classes: " << sched.numCourses
+		<< "-----------------------------------------\n";
+
+	// course description imported from course.cpp ostream operator
+	for (int i = 0; i < sched.numCourses; i++)
+	{
+		output << sched.Courses[i] << "\n" << endl;
+	}
+	return output;
 }
