2ef70a02e7973c3f43e79f5755c5868e9a01877f
Booooooomy <globalman96@naver.com>
2017-10-28 11:12:17 -0700

    Update CourseSchedule.h

--- CourseSchedule.h
+++ CourseSchedule.h
@@ -1,27 +1,38 @@
 #ifndef COURSESCHEDULE_H
 #define COURSESCHEDULE_H
-#include "semester.h"
 #include <string>
+#include <iostream>
+#include "Semester.h"
+using namespace std;
+
+enum DateStatus { clear = 5, startD = 3, endD = 4, both = 2};
 
 class CourseSchedule
 {
-private:
-	static int numCourses;
-	Course* Courses;
-	int maxSize;
-	string sname; // student name
-	Semester smester;
-	Date startDate;
-	Date endDate;
-	void checkDates(Semester, Date, Date);
+	friend ostream &operator<<(ostream &, const CourseSchedule &);
 
-public:
-	CourseSchedule(string, Semester, int);
-	~CourseSchedule();
-	string getStudentName() const;
-	Semester getSemester() const;
-	int getnumCourse() const;
-	void setStudentName(string);
+	private:
+		static int numCourses;
+		int maxSize;
+		Course* Courses;
+		//course(string, string, string, double, Date, Date, Time, Time);
+		//I don't think we need additional course data type as private member. 
+
+		string sname; // student name
+		Semester smester;
+		Date startDate;
+		Date endDate;
+		int checkDates(Semester, Date, Date);      //How do we call course private member variables?
+
+	public:
+		CourseSchedule(string, Semester, int);
+		~CourseSchedule();
+		string getStudentName() const;
+		Semester getSemester() const;
+		int getnumCourse() const;
+		void setStudentName(string);
+		void addCourse(Course&, Semester, Date, Date);
+		void removeCourse();
 };
 
 #endif COURSESHEDULE_H
