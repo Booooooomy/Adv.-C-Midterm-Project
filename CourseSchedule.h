fc03b04751a22fe093a31d968870ddbd18611e45
Booooooomy <globalman96@naver.com>
2017-10-29 16:52:28 -0700

    Update CourseSchedule.h

--- CourseSchedule.h
+++ CourseSchedule.h
@@ -5,31 +5,32 @@
 #include "Semester.h"
 using namespace std;
 
-enum DateStatus { clear = 5, startD = 3, endD = 4, both = 2};
+enum DateStatus { clear = 5, startD = 3, endD = 4, both = 2 };
 
 class CourseSchedule
 {
 	friend ostream &operator<<(ostream &, const CourseSchedule &);
 
-	private:
-		static int numCourses;
-		const int maxSize;
-		Course* Courses;
-		string sname; // student name
-		Semester smester;
-		Date startDate;
-		Date endDate;
-		int checkDates(Semester, Date, Date); 
+private:
+	int numCourses; //doesn't need to be static because we will have only one class for CourseSchedule...
+	int maxSize;
+	Course* Courses;
 
-	public:
-		CourseSchedule(string, Semester, int);
-		~CourseSchedule();
-		string getStudentName() const;
-		Semester getSemester() const;
-		int getnumCourse() const;
-		void setStudentName(string);
-		void addCourse(Course&, Semester, Date, Date);
-		void removeCourse();
+	string sname; // student name
+	Semester smester;
+	Date startDate;
+	Date endDate;
+	int checkDates(Semester, Date, Date);
+
+public:
+	CourseSchedule(string, Semester, int);
+	~CourseSchedule();
+	string getStudentName() const;
+	Semester getSemester() const;
+	int getnumCourse() const;
+	void setStudentName(string);
+	void addCourse(Course&, Semester, Date, Date);
+	void removeCourse();
 };
 
 #endif COURSESHEDULE_H
